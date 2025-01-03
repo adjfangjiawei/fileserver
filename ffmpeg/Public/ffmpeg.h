cc0bb5140be9dd7bd632cad6b5899711ba7311bd
adj <3195727347@qq.com>
2025-01-02 22:17:01 -0800

    feat:remove outdated file

--- ffmpeg/Public/ffmpeg.h
+++
@@ -1,3 +0,0 @@
-extern void ffmpeg_main();
-extern void ffmpeg_pull_stream();
-extern void ffmpeg_push_stream();
--- streammediaserver/Public/server.h
+++
@@ -1,4 +0,0 @@
-#ifndef STREAM_SERVER_H
-#define STREAM_SERVER_H
-extern void init_stream_server();
-#endif
